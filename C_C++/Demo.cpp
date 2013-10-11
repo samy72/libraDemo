aedf62a6e7eb18ce2dcf9d9bb70f94edf36423b7
samy72 <samyc.mobile@gmail.com>
2013-10-11 03:02:28 -0700

    Add NONE test

--- C_C++/Demo.cpp
+++ C_C++/Demo.cpp
@@ -4,6 +4,11 @@
 #include <iostream>
 // Libra
 #include <Libra.h>    // C and C++ API
+#include <unistd.h>
+
+#define N_SAMPLES 10
+#define M_SIZE 1024
+#define FLOPCOUNT ((2*M_SIZE-1)*M_SIZE*(long long)M_SIZE)
 
 typedef enum {
 	ALL,
@@ -13,16 +18,87 @@ typedef enum {
 	CLOUD,
 } TypeOfTest;
 
+typedef struct results {
+  unsigned long long duration;
+  double avg;
+  double sigma2;
+} results;
+
+
+unsigned long long nanoDuration(struct timespec *t1, struct timespec *t2) {
+  return ((t1->tv_sec * 1000000000) + t1->tv_nsec) - ((t2->tv_sec * 1000000000) + t2->tv_nsec);
+}
+
+results computeNoAcc(){
+  int i, j, k;
+  float A[M_SIZE][M_SIZE], B[M_SIZE][M_SIZE], C[M_SIZE][M_SIZE], s=0;
+  double gflops;
+  unsigned long long duration;
+  long long flopCount;
+  struct timespec t1, t2;
+  results res;
+
+  printf("***********************************\n\n");
+  printf("SGEMM : BASIC matrix multiplication\n\n");
+  printf("***********************************\n\n");
+
+  memset(&res, 0, sizeof(results));
+
+  // float 1.0 initialization
+
+  for (  i = 0 ; i < M_SIZE ; i++ )
+    for ( j = 0 ; j < M_SIZE ; j++ )
+      A[i][j] =0.0;
+  
+  for ( i = 0 ; i < M_SIZE ; i++ )
+    for ( j = 0 ; j < M_SIZE ; j++ )
+      B[i][j] = 0.0;
+  
+  clock_gettime(CLOCK_MONOTONIC, &t1);
+
+  for ( i = 0 ; i < M_SIZE ; i++ )
+    for ( j = 0 ; j < M_SIZE ; j++ )
+      {
+	for ( k = 0 ; k < M_SIZE ; k++ ) s = s + A[i][k]*B[k][j];
+        C[i][j] = s;
+        s = 0.0;
+      }
+
+  //  sleep(1);
+
+  clock_gettime(CLOCK_MONOTONIC, &t2);
+
+  res.duration = nanoDuration(&t2, &t1);
+  
+  /* Compute a performance measurement. Gigaflop / s. */
+  return res;
+}
+
+int computeNoAccLoop(){
+  results res[N_SAMPLES];
+  double avg = 0;
+  double gflops = 0;
+
+  for(int i=0; i<N_SAMPLES; i++) {
+    res[i] = computeNoAcc();
+    avg = (avg*i + res[i].duration) / (i+1);
+    printf("Total time : %llu%s", res[i].duration, " nanoseconds\n");
+    printf("Average Total time : %le%s", avg, " nanoseconds\n");
+    gflops = FLOPCOUNT / avg / 1e9;
+    printf("Average %g%s", gflops, " GFlop/s.\n");
+  }
+  return 0;
+}
+
 int compute()
 {
-	const int N = 1024;
 	long long flopCount;
 	double startTime, time, gflops;
 
 	gVar A, B, C;
 
 	/* Create two N by N matrices. */
-	A = ones(N);
+	A = ones(M_SIZE);
 	B = A;
 
 	/* warm up phase of the gpu! (to get accurate timing results) */
@@ -38,23 +114,29 @@ int compute()
 	printf("Total time : %g%s", time*1000, " milliseconds\n");
 
 	/* Compute a performance measurement. Gigaflop / s. */
-	flopCount = (2*N-1)*N*(long long)N;
+	flopCount = (2*M_SIZE-1)*M_SIZE*(long long)M_SIZE;
 	gflops = flopCount / time / 1e9;
 	printf("%g%s", gflops, " GFlop/s.\n");
 }
 
 int computeCPU()
 {
+	printf("*********************************\n\n");
+	printf("SGEMM : CPU matrix multiplication\n\n");
+	printf("*********************************\n\n");
+
 	libra_SetCurrentDevice(CPU_DEVICE_1);
-	printf("CPU Computation\n");
 	compute();
 	return 0;
 }
 
 int computeGPU()
 {
+	printf("*********************************\n\n");
+	printf("SGEMM : GPU matrix multiplication\n\n");
+	printf("*********************************\n\n");
+
 	libra_SetCurrentDevice(GPU_DEVICE_1);
-	printf("GPU Computation\n");
 	compute();
 	return 0;
 }
@@ -69,15 +151,14 @@ int main(int argc, char** argv)
 {
 	TypeOfTest tot = ALL;
 
-	printf("*****************************\n\n");
-	printf("SGEMM : matrix multiplication\n\n");
-	printf("*****************************\n\n");
-	
 	if (2 == argc){
 	  if (!strcmp(argv[1], "CPU")) tot = CPU;
+	  else if (!strcmp(argv[1], "NONE")) tot = NONE;
 	  else if (!strcmp(argv[1], "GPU")) tot = GPU;
 	  else if (!strcmp(argv[2], "CLOUD")) tot = CLOUD;
 	}
+
+	if((ALL==tot) || (NONE==tot)) computeNoAccLoop();
 
 	if (libra_Init(argc, argv) != 0)
 		return 1;
